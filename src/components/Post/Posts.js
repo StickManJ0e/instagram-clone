a7de9f87e6f56c3f0aede7c65fb2989f86b94420
StickManJ0e <121269062+StickManJ0e@users.noreply.github.com>
2023-04-23 16:24:36 +1000

    Load more posts on scroll to end

--- src/components/Post/Posts.js
+++ src/components/Post/Posts.js
@@ -1,17 +1,137 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { firestore } from "../../firebase";
+import { collection, orderBy, getDocs, query, limit, startAfter, onSnapshot, getCountFromServer } from "firebase/firestore";
+import '../../styles/post/Posts.css'
 
 const Posts = (props) => {
     const { currentPosts, setCurrentPosts } = props;
+    const [key, setKey] = useState();
+    const [endLoad, setEndLoad] = useState();
+    const postRef = collection(firestore, 'posts');
+    let location = useLocation();
+    let postObject = (docId, docData) => {
+        return {
+            docId, docData,
+        }
+    }
+
+    //Filter a given array for unqiue posts based of docId
+    const filterArrayWithId = (array) => {
+        if (array.length > 0) {
+            const filteredData = array.filter((value, index, self) =>
+                self.findIndex(v => v.docId === value.docId) === index
+            );
+            return filteredData;
+        }
+    }
+
+    async function postFirst() {
+        //Get first posts
+        const querySnapshot = await getDocs(query(postRef, orderBy('timestamp', 'desc'), limit(2)));
+        let newArray = currentPosts;
+
+        querySnapshot.forEach((doc) => {
+            newArray.push(postObject(doc.id, doc.data()))
+            setKey(doc.data().timestamp);
+        });
+
+        setCurrentPosts(filterArrayWithId(newArray));
+        queryListener();
+    };
+
+    //Fetch the next posts starting from last
+    async function fetchData() {
+        const snapShotCount = await getCountFromServer(postRef);
+        if ((snapShotCount.data().count - currentPosts.length) === 1) {
+            setEndLoad(<div>Reached End</div>);
+        };
+
+        queryListener();
+        const querySnapshot = await getDocs(query(postRef, orderBy('timestamp', 'desc'), startAfter(key), limit(2)));
+        let newArray = currentPosts;
+
+        querySnapshot.forEach((doc) => {
+            newArray.push(postObject(doc.id, doc.data()));
+            setKey(doc.data().timestamp);
+        });
+
+        setCurrentPosts(newArray);
+        queryListener();
+
+    };
+
+    const queryListener = () => {
+        //Listen for new posts created
+        onSnapshot(query(postRef), (snapshot) => {
+            snapshot.docChanges().forEach((change) => {
+                let index = currentPosts.findIndex((post) => post.docId === change.doc.id);
+                //When doc is removed
+                if (change.type === 'removed' && index !== -1) {
+                    let newArray = currentPosts;
+                    newArray.splice(index, 1);
+                    setCurrentPosts(filterArrayWithId(newArray));
+                }
+
+                if (change.type === 'modified' && index === -1) {
+                    //When doc is created 
+                    let posts = currentPosts;
+                    posts = posts.unshift(postObject(change.doc.id, change.doc.data()));
+                    setCurrentPosts(filterArrayWithId(posts));
+                }
+
+                //When doc is modified 
+                if (change.type === 'modified' && index !== -1) {
+                    let posts = currentPosts;
+                    posts[index] = postObject(change.doc.id, change.doc.data());
+                    setCurrentPosts(posts);
+                }
+            });
+        });
+    }
+
+    const handleScroll = () => {
+        if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+            let button = document.querySelector('#load-more');
+            button.click();
+        }
+    }
+
+    const getAspectRatio = (aspectRatio) => {
+        let adjustedAspectRation = (aspectRatio === 'orginal') ? { aspectRatio: 'auto' }
+            : (aspectRatio === '1:1') ? { aspectRatio: '1/1' }
+                : (aspectRatio === '4:5') ? { aspectRatio: '4/5' }
+                    : { aspectRatio: '16/9' };
+
+        return adjustedAspectRation;
+    }
+
+    useEffect(() => {
+        setCurrentPosts([]);
+        postFirst();
+
+        document.addEventListener('scroll', handleScroll);
+
+        return () => {
+            document.removeEventListener('scroll', handleScroll);
+        }
+    }, []);
 
     return (
-        <div className="posts-div">
-            {(currentPosts.length > 0) ? currentPosts.map((post) => {
-                return (
-                    <div key={post.docId}>{post.docId}</div>
-                )
-            })
+        <div>
+            <div className="posts-div">
+                {(currentPosts.length > 0) ? currentPosts.map((post) => {
+                    return (
+                        <div key={post.docId} id={post.docId}>
+                            <img className="post-image" src={post.docData.fileUrl} alt="post" style={getAspectRatio(post.docData.aspectRatio)}></img>
+                        </div>
+                    )
+                })
 
-                : <div></div>}
+                    : <div></div>}
+            </div>
+            {endLoad}
+            <button id="load-more" onClick={() => fetchData()}>Load More</button>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useAuthContext } from "../context/AuthContext";
-import { firestore } from "../firebase";
-import { collection, orderBy, getDocs, query, limit, startAfter, onSnapshot, serverTimestamp, Timestamp } from "firebase/firestore";
 import Navbar from "../components/Navbar/Navbar";
 import Posts from "../components/Post/Posts";
 import '../styles/Home/Home.css'
@@ -10,96 +8,13 @@ const Home = () => {
     const { loggedIn, userData } = useAuthContext();
     const [currentPopUp, setCurrentPopUp] = useState();
     const [currentPosts, setCurrentPosts] = useState([]);
-    const [key, setKey] = useState();
     let user = userData;
-    const postRef = collection(firestore, 'posts');
-
-    const filterArrayWithId = (array) => {
-        if (array.length > 0) {
-            const filteredData = array.filter((value, index, self) =>
-                self.findIndex(v => v.docId === value.docId) === index
-            );
-            return filteredData;
-        }
-    }
-
-    let postObject = (docId, docData) => {
-        return {
-            docId, docData,
-        }
-    }
-
-    async function postFirst() {
-        //Get first posts
-        const querySnapshot = await getDocs(query(postRef, orderBy('timestamp', 'desc'), limit(2)));
-        let newArray = currentPosts;
-
-        querySnapshot.forEach((doc) => {
-            newArray.push(postObject(doc.id, doc.data()))
-            setKey(doc.data().timestamp);
-        });
-
-        setCurrentPosts(filterArrayWithId(newArray));
-        queryListener();
-    };
-
-    async function fetchData() {
-        queryListener();
-        const querySnapshot = await getDocs(query(postRef, orderBy('timestamp', 'desc'), startAfter(key), limit(2)));
-        let newArray = currentPosts;
-
-        querySnapshot.forEach((doc) => {
-            newArray.push(postObject(doc.id, doc.data()));
-            setKey(doc.data().timestamp);
-        });
-
-        setCurrentPosts(newArray);
-        queryListener();
-    };
-
-    const queryListener = () => {
-        //Listen for new posts created
-        onSnapshot(query(postRef), (snapshot) => {
-            snapshot.docChanges().forEach((change) => {
-                let index = currentPosts.findIndex((post) => post.docId === change.doc.id);
-                //When doc is removed
-                if (change.type === 'removed' && index !== -1) {
-                    let newArray = currentPosts;
-                    console.log('removed', newArray);
-                    newArray.splice(index, 1);
-                    setCurrentPosts(filterArrayWithId(newArray));
-                }
-
-                if (change.type === 'modified' && index === -1) {
-                    //When doc is created 
-                    let posts = currentPosts;
-                    console.log('created', posts);
-                    posts = posts.unshift(postObject(change.doc.id, change.doc.data()));
-                    setCurrentPosts(filterArrayWithId(posts));
-                }
-
-                //When doc is modified 
-                if (change.type === 'modified' && index !== -1) {
-                    let posts = currentPosts;
-                    console.log('modified', posts);
-                    posts[index] = postObject(change.doc.id, change.doc.data());
-                    setCurrentPosts(posts);
-                }
-            });
-        });
-    }
-
-    useEffect(() => {
-        setCurrentPosts([]);
-        postFirst();
-    }, []);
 
     return (
         <div id="main-wrapper">
             <Navbar setCurrentPopUp={setCurrentPopUp} />
             <div id="main">
                 <Posts currentPosts={currentPosts} setCurrentPosts={setCurrentPosts} />
-                <button onClick={() => fetchData()}>Load More</button>
             </div>
             {currentPopUp}
         </div>
