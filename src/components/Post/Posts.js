a2987a2c171e3ddc59c96327ea1f042de7331278
StickManJ0e <121269062+StickManJ0e@users.noreply.github.com>
2023-04-23 11:10:00 +1000

    Load post data and update when detect changes

--- src/components/Post/Posts.js
+++ src/components/Post/Posts.js
@@ -1,8 +1,18 @@
 import React from "react";
 
-const Posts = () => {
+const Posts = (props) => {
+    const { currentPosts } = props;
+
     return (
-        <div>Posts</div>
+        <div className="posts-div">
+            {(currentPosts.length > 0) ? currentPosts.map((post) => {
+                return (
+                    <div key={post.docId}>{post.docId}</div>
+                )
+            })
+
+                : <div></div>}
+        </div>
     )
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuthContext } from "../context/AuthContext";
+import { firestore } from "../firebase";
+import { collection, orderBy, getDocs, query, limit, startAfter, onSnapshot, serverTimestamp, Timestamp } from "firebase/firestore";
 import Navbar from "../components/Navbar/Navbar";
 import Posts from "../components/Post/Posts";
 import '../styles/Home/Home.css'
@@ -7,19 +9,80 @@ import '../styles/Home/Home.css'
 const Home = () => {
     const { loggedIn, userData } = useAuthContext();
     const [currentPopUp, setCurrentPopUp] = useState();
+    const [currentPosts, setCurrentPosts] = useState([]);
+    const [pastPosts, setPastPosts] = useState([]);
+    const [key, setKey] = useState();
     let user = userData;
+    const postRef = collection(firestore, 'posts');
 
-    const test = () => {
-        console.log(user.displayName);
-        console.log(loggedIn);
+    let postObject = (docId, docData) => {
+        return {
+            docId, docData,
+        }
     }
+
+    async function postFirst() {
+        //Get first posts
+        const querySnapshot = await getDocs(query(postRef, orderBy('timestamp', 'desc'), limit(2)));
+        let newArray = currentPosts;
+
+        querySnapshot.forEach((doc) => {
+            newArray.push(postObject(doc.id, doc.data()))
+            setKey(doc.data().timestamp);
+        });
+
+        setCurrentPosts(newArray);
+
+        //Listen for new posts created
+        onSnapshot(query(postRef), (snapshot) => {
+            setPastPosts(currentPosts);
+            snapshot.docChanges().forEach((change) => {
+                if (change.type === 'removed') {
+                    let index = currentPosts.findIndex((post) => post.docId === change.doc.id);
+                    let newArray = currentPosts;
+                    newArray.splice(index, 1);
+                    setCurrentPosts(newArray);
+                }
+                if (change.type === 'modified') {
+                    let index = currentPosts.findIndex((post) => post.docId === change.doc.id);
+                    if (index === -1) {
+                        let posts = currentPosts;
+                        posts = posts.unshift(postObject(change.doc.id, change.doc.data()));
+                        setCurrentPosts(posts);
+                    }
+                    else {
+                        let posts = currentPosts;
+                        posts[index] = postObject(change.doc.id, change.doc.data());
+                        setCurrentPosts(posts);
+                    }
+                };
+            });
+        });
+    };
+
+    async function fetchData() {
+        const querySnapshot = await getDocs(query(postRef, orderBy('timestamp', 'desc'), startAfter(key), limit(2)));
+        let newArray = currentPosts;
+
+        querySnapshot.forEach((doc) => {
+            newArray.push(postObject(doc.id, doc.data()));
+            setKey(doc.data().timestamp);
+            console.log(doc.id);
+        });
+
+        setCurrentPosts(newArray)
+    };
+
+    useEffect(() => {
+        return () => postFirst();
+    }, []);
 
     return (
         <div id="main-wrapper">
             <Navbar setCurrentPopUp={setCurrentPopUp} />
             <div id="main">
-                <Posts />
-                {/* <button onClick={() => test()}>Test</button> */}
+                <Posts currentPosts={currentPosts} pastPosts={pastPosts}/>
+                <button onClick={() => fetchData()}>Load More</button>
             </div>
             {currentPopUp}
         </div>
